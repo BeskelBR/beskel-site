ebb4f0443f9fcbea99599196d67662c00ae3aa54
BeskelBR <mateus.fis.unb@gmail.com>
2026-09-17 14:37:39 -0300

    fix(site): separar destinos do sistema e terminal HVB

--- config/site.js
+++ config/site.js
@@ -11,7 +11,8 @@ window.HVB_SITE_CONFIG = {
   urls: {
     publicDev: "https://hvb-site-dev.beskel.com.br",
     collaborator: "/colaborador",
-    systemDev: "https://hvb-dev.beskel.com.br",
+    systemDev: "https://hvb-sistema-dev.beskel.com.br",
+    terminalDev: "https://hvb-dev.beskel.com.br",
     publicFuture: "https://www.hvb.com.br",
     systemFuture: "https://sistema.hvb.com.br"
   },
