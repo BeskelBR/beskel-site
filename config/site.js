5d7af83b5b76c0760cddbfd93d9bc5db15305d69
BeskelBR <mateus.fis.unb@gmail.com>
2026-09-17 13:01:39 -0300

    feat(site): definir gateway da área do colaborador

--- config/site.js
+++ config/site.js
@@ -10,6 +10,7 @@ window.HVB_SITE_CONFIG = {
   },
   urls: {
     publicDev: "https://hvb-site-dev.beskel.com.br",
+    collaborator: "/colaborador",
     systemDev: "https://hvb-dev.beskel.com.br",
     publicFuture: "https://www.hvb.com.br",
     systemFuture: "https://sistema.hvb.com.br"
