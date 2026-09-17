5b7293c640ace589e62a3f0e2ebc7cbd783a150d
BeskelBR <mateus.fis.unb@gmail.com>
2026-09-17 14:37:54 -0300

    Padroniza contatos oficiais da BESKEL

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,19 +4,11 @@ window.BESKEL_CONFIG = Object.freeze({
   businessName: "BESKEL",
   domain: "https://beskel.com.br",
   whatsapp: "5561991668921",
+  phoneDisplay: "(61) 99166-8921",
+  phoneHref: "tel:61991668921",
   email: "contato@beskel.com.br",
   instagramUser: "beskelbr",
   instagramUrl: "https://instagram.com/beskelbr",
   city: "Brasília",
   state: "DF"
 });
-
-// Carrega a camada visual do Brand Book também nas páginas legadas.
-if (!document.querySelector('link[href*="brand-book.css"]')) {
-  const brandStyles = document.createElement("link");
-  brandStyles.rel = "stylesheet";
-  brandStyles.href = document.currentScript?.src.includes("/pages/")
-    ? "../assets/css/brand-book.css"
-    : "assets/css/brand-book.css";
-  document.head.appendChild(brandStyles);
-}
